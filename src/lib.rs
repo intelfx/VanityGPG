5c142c8bff6ea9cb5a2ab6e3374099ed4474ade0
Ivan Shapovalov <intelfx@intelfx.name>
2025-07-13 21:44:26 +0300

    WIP: update scoring function again

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@
 //! let backend = DefaultBackend::new(CipherSuite::Curve25519).unwrap();
 //! println!("Fingerprint: {}", backend.fingerprint());
 //! ```
+#![allow(dead_code)]
+#![allow(unused)]
 
 extern crate anyhow;
 extern crate byteorder;
@@ -31,6 +33,8 @@ pub use pgp_backends::RPGPBackend;
 #[cfg(feature = "sequoia")]
 pub use pgp_backends::SequoiaBackend;
 pub use pgp_backends::{ArmoredKey, Backend, CipherSuite, DefaultBackend, UserID};
+
+use std::iter::zip;
 
 #[derive(Copy, Clone)]
 pub enum Match<T> {
@@ -101,22 +105,23 @@ pub fn score(fingerprint: &str) -> Match<u32> {
     let c = &fpr[len-4..len-2];
     let d = &fpr[len-2..len-0];
 
-    let fpr2 = &fpr[0..len-8];
-    let len2 = fpr2.len();
-    let xa = &fpr2[len2-8..len2-6];
-    let xb = &fpr2[len2-6..len2-4];
-    let xc = &fpr2[len2-4..len2-2];
-    let xd = &fpr2[len2-2..len2-0];
+    let xfpr = &fpr[0..len-8];
+    let xlen = xfpr.len();
+    let xa = &xfpr[xlen-8..xlen-6];
+    let xb = &xfpr[xlen-6..xlen-4];
+    let xc = &xfpr[xlen-4..xlen-2];
+    let xd = &xfpr[xlen-2..xlen-0];
 
+    /*
     if let Match::Yes(m1) = score_magic(fpr) {
         let mut score = m1;
 
-        if let Match::Yes(m2) = score_magic(fpr2) {
+        if let Match::Yes(m2) = score_magic(xfpr) {
             score += 1024 * m2;
         }
         else if (xa[0] == a[0] && xb[0] == a[0] && xc[0] == a[0] && xd[0] == a[0])
              || (xa[1] == a[1] && xb[1] == a[1] && xc[1] == a[1] && xd[1] == a[1]) {
-            if let Match::Yes(m2) = score_pattern(fpr2) {
+            if let Match::Yes(m2) = score_pattern(xfpr) {
                 score += 1024 * m2;
             }
             else {
@@ -125,7 +130,7 @@ pub fn score(fingerprint: &str) -> Match<u32> {
         }
         else if (xa == a || xb == b || xc == c || xd == d)
              || (           xb == a || xc == a || xd == a) {
-            if let Match::Yes(m2) = score_pattern(fpr2) {
+            if let Match::Yes(m2) = score_pattern(xfpr) {
                 score += 1024 * m2;
             }
         }
@@ -136,7 +141,7 @@ pub fn score(fingerprint: &str) -> Match<u32> {
 
         if (a[0] == xd[0] && b[0] == xd[0] && c[0] == xd[0] && d[0] == xd[0])
         || (a[1] == xd[1] && b[1] == xd[1] && c[1] == xd[1] && d[1] == xd[1]) {
-            if let Match::Yes(m2) = score_magic(fpr2) {
+            if let Match::Yes(m2) = score_magic(xfpr) {
                 score += 1024 * m2;
             }
             else {
@@ -145,7 +150,64 @@ pub fn score(fingerprint: &str) -> Match<u32> {
         }
         else if (xa == a || xb == b || xc == c || xd == d)
              || (xa == d || xb == d || xc == d           ) {
-            if let Match::Yes(m2) = score_magic(fpr2) {
+            if let Match::Yes(m2) = score_magic(xfpr) {
+                score += 1024 * m2;
+            }
+        }
+        return Match::Yes(score);
+    }
+     */
+/*
+    if let Match::Yes(m1) = score_magic(xfpr) {
+        let mut score = m1;
+
+        if (a[0] == xd[0] && b[0] == xd[0] && c[0] == xd[0] && d[0] == xd[0])
+        || (a[1] == xd[1] && b[1] == xd[1] && c[1] == xd[1] && d[1] == xd[1]) {
+            if let Match::Yes(m2) = score_pattern(fpr) {
+                score += 1024 * m2;
+            }
+            else {
+                score += 1024;
+            }
+        }
+        else if (xa == a || xb == b || xc == c || xd == d)
+             || (xa == d || xb == d || xc == d           ) {
+            if let Match::Yes(m2) = score_pattern(fpr) {
+                score += 1024 * m2;
+            }
+        }
+        return Match::Yes(score);
+    }
+*/
+
+    if !(fpr.ends_with("A5A5E5E5".as_bytes()) ||
+         fpr.ends_with("A5E5A5E5".as_bytes()) ||
+         fpr.ends_with("E5E5A5A5".as_bytes()) ||
+         fpr.ends_with("E5A5E5A5".as_bytes())) {
+        return Match::No;
+    }
+
+    if let Match::Yes(m1) = score_pattern(fpr) {
+        let mut score = m1;
+
+        if (a[0] == xd[0] && b[0] == xd[0] && c[0] == xd[0] && d[0] == xd[0])
+        || (a[1] == xd[1] && b[1] == xd[1] && c[1] == xd[1] && d[1] == xd[1]) {
+            if let Match::Yes(m2) = score_magic(xfpr) {
+                score += 1024 * m2;
+            }
+        }
+        else if (xa == a || xb == b || xc == c || xd == d)
+             || (xa == d || xb == d || xc == d           ) {
+            if let Match::Yes(m2) = score_magic(xfpr) {
+                score += 1024 * m2;
+            }
+        }
+        else if ((xa[0] == xb[0] && xb[0] == xc[0] && xc[0] == xd[0] && xd[0] == a[0] && a[0] == b[0] && b[0] == c[0] && c[0] == d[0])
+              || (xa[1] == xb[1] && xb[1] == xc[1] && xc[1] == xd[1] && xd[1] == a[1] && a[1] == b[1] && b[1] == c[1] && c[1] == d[1]))
+             && ((xc == a && xd == b)
+              || (xc == xd && a == b)
+              || (xd == a)) {
+            if let Match::Yes(m2) = score_pattern(xfpr) {
                 score += 1024 * m2;
             }
         }
@@ -156,7 +218,7 @@ pub fn score(fingerprint: &str) -> Match<u32> {
 }
 
 fn score_magic(fpr: &[u8]) -> Match<u32> {
-    let mut score= 0;
+    let mut score = 0;
 
     let len = fpr.len();
     // let a = &fpr[len-8..len-6];
@@ -172,7 +234,7 @@ fn score_magic(fpr: &[u8]) -> Match<u32> {
 
     for &magic in MAGIC2 {
         if abcd.ends_with(magic) {
-            return Match::Yes(512);
+            return Match::Yes(1024);
         }
     }
 
@@ -186,9 +248,9 @@ fn score_magic(fpr: &[u8]) -> Match<u32> {
     // if has_magic(0b0110) && xcd == ab { return Match::No; }
     if has_magic(0b0011) && ab == cd { return Match::No; }
     match is_magic {
-        // 0b1111 => return Match::Yes(1024),
-        // 0b0111 => return Match::Yes(768),
-        0b0011 => return Match::Yes(512),
+        // 0b1111 => return Match::Yes(4096),
+        // 0b0111 => return Match::Yes(2048),
+        0b0011 => return Match::Yes(1024),
         0b0000 => (),
         _      => return Match::No,
     }
@@ -197,7 +259,7 @@ fn score_magic(fpr: &[u8]) -> Match<u32> {
 }
 
 fn score_pattern(fpr: &[u8]) -> Match<u32> {
-    let mut score= 0;
+    let mut score = 0;
 
     let len = fpr.len();
 
@@ -208,10 +270,10 @@ fn score_pattern(fpr: &[u8]) -> Match<u32> {
 
     fn apply<F>(fpr: &[u8], len: usize, mut op: F)
     where
-        F: FnMut(&[u8; 2]),
+        F: FnMut(&[u8; 2]) -> bool,
     {
-        for x in fpr[0..len - 8].chunks_exact(2) {
-            op(x.try_into().unwrap());
+        for x in fpr[0..len - 8].chunks_exact(2).rev() {
+            if !op(x.try_into().unwrap()) { break; }
         }
     }
 
@@ -219,44 +281,67 @@ fn score_pattern(fpr: &[u8]) -> Match<u32> {
     where
         F: Fn(&[u8; 2]) -> bool,
     {
-        apply(fpr, len, |x| if matcher(x.try_into().unwrap()) { *score += adj; });
+        apply(fpr, len, |x| if matcher(x.try_into().unwrap()) { *score += adj; true } else { false });
     }
 
-    if a[0] == b[0] && b[0] == c[0] && c[0] == d[0] {
-        apply_p(&fpr, len, &mut score, |arg| arg[0] == a[0], 1);
-    } else if a[1] == b[1] && b[1] == c[1] && c[1] == d[1] {
-        apply_p(&fpr, len, &mut score, |arg| arg[1] == a[1], 1);
-    } else {
-        return Match::No;
+    fn is_all4<T>(a: T, b: T, c: T, d: T) -> bool
+    where T: Eq
+    {
+        a == b && b == c && c == d
     }
 
-    if a == b && b == c && c == d {
+    fn is_pairwise<T>(a: T, b: T, c: T, d: T) -> bool
+    where T: Eq
+    {
+        a == b && c == d || a == c && b == d
+    }
+
+    if is_all4(a, b, c, d) {
         score += 256;
-        apply_p(&fpr, len, &mut score, |arg| arg == a, 2);
-    } else if a == b && c == d || a == c && b == d {
-        assert_ne!(a, d);
+        apply_p(&fpr, len, &mut score, |arg| arg == a, 1024);
+        return Match::Yes(score);
+
+    } else if is_pairwise(a, b, c, d) {
         score += 128;
-        let mut scores = [score; 2];
-        for x in fpr[0..len-8].chunks_exact(2) {
-                 if x == a { scores[0] += 2; }
-            else if x == d { scores[1] += 2; }
+        let mut scores = [score; 5];
+
+        if is_all4(a[0], b[0], c[0], d[0]) {
+            apply_p(&fpr, len, &mut scores[0], |arg| arg[0] == a[0], 1024);
+        } else if is_all4(a[1], b[1], c[1], d[1]) {
+            apply_p(&fpr, len, &mut scores[0], |arg| arg[1] == a[1], 1024);
+        } else {
+            return Match::No;
         }
+
+        for (x, this_score) in zip([a, b, c, d], &mut scores[1..]) {
+            apply_p(&fpr, len, this_score, |arg| arg == x, 1024);
+        }
+
         score = scores.into_iter().max().unwrap();
-    } else if a == b || c == d || a == c || b == d {
+        return Match::Yes(score);
+
+/*    } else if a == b || c == d || a == c || b == d {
         score += 64;
-        let mut scores = [score; 4];
-        for x in fpr[0..len-8].chunks_exact(2) {
-                 if x == a { scores[0] += 2; }
-            else if x == b { scores[1] += 2; }
-            else if x == c { scores[2] += 2; }
-            else if x == d { scores[3] += 2; }
+
+        if is_all4(a[0], b[0], c[0], d[0]) {
+            apply_p(&fpr, len, &mut score, |arg| arg[0] == a[0], 1024);
+        } else if is_all4(a[1], b[1], c[1], d[1]) {
+            apply_p(&fpr, len, &mut score, |arg| arg[1] == a[1], 1024);
+        } else {
+            return Match::No;
         }
+
+        let mut scores = [score; 4];
+        for (x, this_score) in zip([a, b, c, d], &mut scores) {
+            apply_p(&fpr, len, this_score, |arg| arg == x, 1024);
+        }
+
         score = scores.into_iter().max().unwrap();
-    } else {
-        return Match::No;
+        return Match::Yes(score);
+*/
     }
 
-    Match::Yes(score)
+    Match::No
 }
 
 #[cfg(test)]
